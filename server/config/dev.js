db2c16811e75a79d04f34fc95a5b4dc6ec1b732a
Vashisth00 <vashisthbhushan@gmail.com>
2020-12-22 22:05:33 +0530

    Mongo Connection and Login done

---
+++ server/config/dev.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI:'mongodb+srv://vashisth:vashisth@cluster0.r5f4j.mongodb.net/vashisth?retryWrites=true&w=majority'
+}
--- server/index.js
+++ server/index.js
@@ -53,7 +53,7 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5001
 
 app.listen(port, () => {
   console.log(`Server Listening on ${port}`)
