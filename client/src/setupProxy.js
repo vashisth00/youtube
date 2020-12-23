8cecfdaa3682d939f0e2623528a4aa8bf8e64f6d
Vashisth00 <vashisthbhushan@gmail.com>
2020-12-24 04:31:29 +0530

    Proxy fix

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,8 +1,8 @@
-const proxy = require("http-proxy-middleware");
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
 
-    app.use(proxy("/api", { target: "http://localhost:8080/",
+    app.use(createProxyMiddleware("/api", { target: "http://localhost:8080/",
     changeOrigin: true, }),);
 
 };
