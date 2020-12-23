2d9b91c022e6afce899ff32dc8883a1f25381898
Vashisth00 <vashisthbhushan@gmail.com>
2020-12-24 04:41:10 +0530

    styles

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -14,7 +14,7 @@ function App() {
   return (
     <Suspense fallback={(<div>Loading...</div>)}>
       <NavBar />
-      <div style={{ paddingTop: '75px', minHeight: 'calc(100vh - 80px)' }}>
+      <div style={{    backgroundColor: '#fbfae4', paddingTop: '135px', minHeight: 'calc(100vh - 80px)' }}>
         <Switch>
           <Route exact path="/" component={Auth(LandingPage, null)} />
           <Route exact path="/login" component={Auth(LoginPage, false)} />
